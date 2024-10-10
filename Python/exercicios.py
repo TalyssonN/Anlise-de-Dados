eb135f1c3326c0b462a298f4ac965d406c742749
Talysson <110793575+TalyssonN@users.noreply.github.com>
2024-10-10 10:56:11 -0300

    Python

---
+++ Python/exercicios.py
@@ -0,0 +1,29 @@
+# # APRENDENDO PYTHON
+# #1. Faça um programa que imprima "Estou aprendendo Python!" na tela.
+# letra = "Estou aprendendo python!"
+# print(letra)
+
+# # 2. Faça um programa que peça ao usuário para digitar um número e, em seguida, imprima na tela a mensagem "O número digitado foi <numero>"
+# solicitarN = int(input("Digite um número: "))
+# print("O número digitado foi:", solicitarN)
+
+# # 3. Faça um programa que peça ao usuário para digitar seu nome e, em seguida, imprima na tela a mensagem "Olá, <nome>!"
+# solicitarNome = str(input("Digite seu nome: "))
+# print("Olá,",solicitarNome)
+
+# # 4. Faça um programa receba algo digitado pelo usuário e verifique se ele digitou alguma coisa, retornando True caso tenha e False caso não tenha.
+# solicitarConteudo = str(input("Digite algo: "))
+# if solicitarConteudo == '':
+#     print("True")
+# else:
+#     print("False")
+
+# 5.  Faça um programa que receba os seguintes dados de um funcionário: nome, idade e salario. Na empresa que esse funcionário trabalha, seu salário é aumentado de ano em ano em R$ 800,90.
+
+# nomeFuncionario = str(input("Digite seu nome: "))
+# idadeFuncionario = int(input("Digite sua idade: "))
+# salarioFuncionario = int(input("Digite seu salário atual, somente números: "))
+# print("Nome:{}, idade:{}, salário no próximo ano:{}".format(nomeFuncionario, idadeFuncionario, salarioFuncionario + 800))
+
+
+
